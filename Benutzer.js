978231de3283a1d934363ad7a6751be727a7100b
Kevin <Kevin@89.27.128.75>
2017-11-29 15:53:32 +0100

    Benutzer + Veranstaltung

---
+++ Benutzer.js
@@ -0,0 +1,9 @@
+function Benutzer(id, name, vorname, passwort, email, alter, profilbild){
+	this.id=id;
+	this.name=name;
+	this.vorname=vorname;
+	this.passwort=passwort;
+	this.email=email;
+	this.alter=alter;
+	this.profilbild=profilbild;
+}
---
+++ Veranstaltung.js
@@ -0,0 +1,11 @@
+function Veranstaltung(id, name, datum, uhrzeit, kategorie, beschreibung ersteller, teilnehmer){
+	this.id=id;
+	this.name=name;
+	this.datum=datum;
+	this.uhrzeit=uhrzeit;
+	this.kategorie=kategorie;
+	this.beschreibung=beschreibung;
+	this.ersteller=ersteller;
+	this.teilnehmer=teilnehmer;
+	
+}
