732563de117b7459404464dec8f2216db87b86fe
Kevin <Kevin@89.27.128.126>
2017-12-11 20:18:09 +0100

    Registrierung

--- checkRegistrierung.js
+++ checkRegistrierung.js
@@ -1,29 +1,48 @@
 function checkEmail(){
 var emailToCheck = document.getElementById("email").value;
 
-if(emailToCheck.match(!"@stud.hshl.de")){
-document.getElementById ("email").innerHTML = "Email nicht korrekt";
-document.getElementById ("email").style.color = "#ff0000";
+     if(emailToCheck.length == 0){
+	document.getElementById("emailtext").textContent="Keine E-Mail eingegeben!";
+	document.getElementById ("emailtext").style.color = "#ff0000";
+     }
+     else if(emailToCheck.indexOf(".")==-1){
+		 document.getElementById("emailtext").textContent="Email nicht korrekt!"
+		document.getElementById ("emailtext").style.color = "#ff0000";
 }
-else if(emailToCheck.IndexOf = ""){
-	document.getElementById("email").innerHTML="Keine E-Mail eingegeben!"
+else{
+	document.getElementById("emailtext").textContent="Email korrekt!"
+	document.getElementById ("emailtext").style.color = "#008000";
 }
-
 }
 
 function checkPasswort(){
-	var passwortToCheck = document.getElementById("passwort").value;
-	var passwort2ToCheck = document.getElementById("passwortwd").value
-	if (passwortToCheck.IndexOf = ""){
-		document.getElementById("passwort"){
-			document.getElementById("passwort").innerHTML="Kein Passwort eingegeben!";
+	var passwortToCheck = document.getElementById("passr").value;
+	var passwort2ToCheck = document.getElementById("passwd").value;
+	if (passwortToCheck.length == 0){
+			document.getElementById("passtext").textContent="Kein Passwort eingegeben!";
+			document.getElementById ("passtext").style.color = "#ff0000";
 		}
-	}
-	else if(passwortToCheck.IndexOf != passwort2ToCheck.IndexOf){
-		document.getElementById("passwort").innerHTML="Passwörter stimmen nicht überein!";
+	else if(passwortToCheck != passwort2ToCheck){
+		document.getElementById("passtext").textContent="Passwörter stimmen nicht überein!";
+		document.getElementById ("passtext").style.color = "#ff0000";
 		
 	}
-	else if(passwortToCheck..match(/[\\,\[,\],\{,\},\",\',ö,ä,ü,Ö,Ä,Ü,ß]/)){
-		document.getElementById("passwort").innerHTML="Passwort darf keine Sonderzeichen enthalten!"
+	else if(passwortToCheck.match(/[\\,\[,\],\{,\},\",\',ö,ä,ü,Ö,Ä,Ü,ß]/)){
+		document.getElementById("passtext").textContent="Passwort darf keine Sonderzeichen enthalten!";
+		document.getElementById ("passtext").style.color = "#ff0000";
 	}
-}
+	else if (passwortToCheck.length < 8 || passwortToCheck.length >20){
+	    document.getElementById("passtext").textContent="Passwortlänge stimmt nicht!";
+	    document.getElementById ("passtext").style.color = "#ff0000";
+		}
+	else if(passwortToCheck.match(/[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]/)==null){ 
+		document.getElementById("passtext").textContent="Enthält keinen Großbuchstaben!";
+	    document.getElementById ("passtext").style.color = "#ff0000";
+		}
+			
+	else {
+			document.getElementById("passtext").textContent="Passwort ist richtig";
+			document.getElementById ("passtext").style.color = "#008000";
+			}
+		}
+	
